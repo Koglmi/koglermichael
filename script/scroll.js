4c339a4ce3274e52df7024a1aa0a4a8be828fe75
Koglmi <40146720180269@litec.ac.at>
2020-05-02 16:46:42 +0200

    scroll function improve

--- script/scroll.js
+++ script/scroll.js
@@ -1,17 +1,22 @@
 window.addEventListener('load', () => {
+    window.addEventListener('scroll', () => {
+        const nav = document.querySelector('nav');
+        nav.classList.toggle('sticky', window.scrollY > 0);
+    })
+
+    const nav = document.querySelector('nav');
     const homeNav = document.getElementById('homeNav');
     const aboutNav = document.getElementById('aboutNav');
     const skillsNav = document.getElementById('skillsNav');
     const workNav = document.getElementById('workNav');
     const contactNav = document.getElementById('contactNav');
     
-    const nav = document.getElementsByTagName('nav')[0];
     const homeSection = document.getElementById('home');
     const aboutSection = document.getElementById('about');
     const skillsSection = document.getElementById('skills');
     const workSection = document.getElementById('work');
     const contactSection = document.getElementById('contact');
-    
+
     homeNav.addEventListener('click', () => {
         window.scrollTo({
             top: 0,
@@ -20,35 +25,73 @@ window.addEventListener('load', () => {
         });
     });
 
+
     aboutNav.addEventListener('click', () => {
-        window.scrollTo({
-            top: homeSection.clientHeight,
-            left: 0,
-            behavior: 'smooth'
-        })
+        if(window.scrollY > 0) {
+            window.scrollTo({
+                top: homeSection.clientHeight + 2,
+                left: 0,
+                behavior: 'smooth'
+            })
+            // console.log(homeSection.clientHeight);
+        } else {
+            window.scrollTo({
+                top: homeSection.clientHeight - nav.clientHeight + 42,
+                left: 0,
+                behavior: 'smooth'
+            })
+            // console.log(homeSection.clientHeight - nav.clientHeight + 40);
+        }
+        
     })
 
     skillsNav.addEventListener('click', () => {
-        window.scrollTo({
-            top: homeSection.clientHeight + aboutSection.clientHeight,
-            left: 0,
-            behavior: 'smooth'
-        })
+        if (window.scrollY > 0) {
+            window.scrollTo({
+                top: homeSection.clientHeight + aboutSection.clientHeight + 2,
+                left: 0,
+                behavior: 'smooth'
+            })
+        } else {
+            window.scrollTo({
+                top: homeSection.clientHeight + aboutSection.clientHeight - nav.clientHeight + 42,
+                left: 0,
+                behavior: 'smooth'
+            })
+        }
+        
     })
 
     workNav.addEventListener('click', () => {
-        window.scrollTo({
-            top: homeSection.clientHeight + aboutSection.clientHeight + skillsSection.clientHeight,
-            left: 0,
-            behavior: 'smooth'
-        })
+        if (window.scrollY > 0) {
+            window.scrollTo({
+                top: homeSection.clientHeight + aboutSection.clientHeight + skillsSection.clientHeight + 2,
+                left: 0,
+                behavior: 'smooth'
+            })
+        } else {
+            window.scrollTo({
+                top: homeSection.clientHeight + aboutSection.clientHeight + skillsSection.clientHeight - nav.clientHeight + 42,
+                left: 0,
+                behavior: 'smooth'
+            })
+        }
+        
     })
 
     contactNav.addEventListener('click', () => {
-        window.scrollTo({
-            top: homeSection.clientHeight + aboutSection.clientHeight + skillsSection.clientHeight + workSection.clientHeight,
-            left: 0,
-            behavior: 'smooth'
-        })
-    })
+        if (window.scrollY > 0) {
+            window.scrollTo({
+                top: homeSection.clientHeight + aboutSection.clientHeight + skillsSection.clientHeight + workSection.clientHeight + 2,
+                left: 0,
+                behavior: 'smooth'
+            })  
+        } else {
+            window.scrollTo({
+                top: homeSection.clientHeight + aboutSection.clientHeight + skillsSection.clientHeight + workSection.clientHeight - nav.clientHeight + 42,
+                left: 0,
+                behavior: 'smooth'
+            })  
+        }
+    })  
 })
